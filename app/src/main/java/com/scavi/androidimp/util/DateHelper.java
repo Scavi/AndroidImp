59ac1fd14ba90e8cc409b06b4bbdcf35cf610eb9
Michael Heymel <cfa.scavenger@gmx.de>
2018-03-10 11:44:15 +0100

    new helper class to handle dates

---
+++ app/src/main/java/com/scavi/androidimp/util/DateHelper.java
@@ -0,0 +1,50 @@
+package com.scavi.androidimp.util;
+
+import java.util.Calendar;
+
+public class DateHelper {
+    /**
+     * Private constructor to prevent access of the helper class (only static access)
+     */
+    private DateHelper() {
+
+    }
+
+    /**
+     * Determines the start timestamp (based on milli seconds) of the last month depending on the
+     * given timestamp
+     *
+     * @param timeInMillis the given timestamp
+     * @return the start of the last month
+     */
+    public static long startOfLastMonth(final long timeInMillis) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(endOfLastMonth(timeInMillis));
+        c.set(Calendar.DAY_OF_MONTH, 1);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        return c.getTimeInMillis();
+    }
+
+    /**
+     * Determines the start timestamp (based on milli seconds) of the last month depending on the
+     * given timestamp
+     *
+     * @param timeInMillis the given timestamp
+     * @return the start of the last month
+     */
+    public static long endOfLastMonth(final long timeInMillis) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(timeInMillis);
+        c.set(Calendar.DAY_OF_MONTH, 1);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        c.add(Calendar.MILLISECOND, -1);
+        return c.getTimeInMillis();
+    }
+
+}
